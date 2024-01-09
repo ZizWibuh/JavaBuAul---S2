14c2445ad7d55fbe25e5dd7ccb7daa7d246725e2
ZizWibuh <mohabdulaziz614@gmail.com>
2024-01-09 21:13:30 +0700

    OOP Example

---
+++ Car.java
@@ -0,0 +1,23 @@
+package JavaBuAul;
+
+public class Car {
+    private String merk;
+    private int topSpeed;
+    protected String negaraAsal = "Japan";
+
+
+public void setMerk (String merk) {
+    this.merk = merk;
+}
+public String getMerk() {
+    return merk;
+}
+
+public void settopSpeed (int topSpeed) {
+    this.topSpeed = topSpeed;
+}
+public int gettopSpeed() {
+    return topSpeed;
+}
+
+}

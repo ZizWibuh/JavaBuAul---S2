906dc48504f83adb6931a60c91241bbfc063bebf
ZizWibuh <mohabdulaziz614@gmail.com>
2024-01-11 17:39:35 +0700

    Update BolaRun.java

--- Praktikum2/BolaRun.java
+++ Praktikum2/BolaRun.java
@@ -4,19 +4,26 @@ import java.util.Scanner;
 
 public class BolaRun {
     public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-        BolaPraktikum run = new BolaPraktikum();
-
+        //declaration variable
         double inputJariJari;
 
+        //Calling class BolaPraktikum
+        BolaPraktikum run = new BolaPraktikum();
+
+        //declaration Scanner input
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai Jari-jari: ");
+        //input Jari-jari
         inputJariJari = input.nextDouble();
         run.setJariJari(inputJariJari);
 
+        //print diameter, surface area, and volume
         System.out.println("Diameter: " + run.showDiameter());
         System.out.println("Luas permukaan: " + run.showLuasPermukaan());
         System.out.println("Volume: " + run.showVolume());
 
+        //close Scanner input
         input.close();
     }
 }
